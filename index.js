f3c98868c3c998c7d7ecc5009f8c008242bdf89e
Deepanshu Dixit <deepanshu6255@gmail.com>
2024-08-01 10:23:50 +0530

    changes in the function in index.js file

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
-const arr = ['asd', 'lsf', 'htw', 'htek'];
-
-const result = arr.map(val => ({name: val}))
-console.log(result);
+// Function to create and handle a promise
+async function handlePromise(i) {
+    let promise1 = new Promise((resolveOuter) => {
+        resolveOuter(
+            new Promise((resolveInner) => {
+                console.log("inside promise 1", i);
+                setTimeout(() => {
+                    console.log("inside promise", i);
+                    resolveInner();
+                }, 1000);
+            })
+        );
+    });
+    console.log("before", i);
+    await promise1;
+    console.log("After", i);
+}// Function to iterate using a for loop
+async function fn1() {
+    for (let i = 0; i < 5; i++) {
+        await handlePromise(i);
+    }
+}// Test the functions
+(async function testFunctions() {
+    await fn1();
+    console.log('>>>> fn1 completed');})();console.log('>>>> last');// Function to create and handle a promise
+// async function handlePromise(i) {
+//     let promise1 = new Promise((resolveOuter) => {
+//         resolveOuter(
+//             new Promise((resolveInner) => {
+//                 console.log("inside promise 1", i);
+//                 setTimeout(() => {
+//                     console.log("inside promise", i);
+//                     resolveInner();
+//                 }, 1000);
+//             })
+//         );
+//     });
+//     console.log("before", i);
+//     await promise1;
+//     console.log("After", i);
+// }// // Function to iterate using forEach loop
+// async function fn2() {
+//     const array = [0, 1, 2, 3, 4];
+//     array.forEach(async (i) => {
+//         await handlePromise(i);
+//     });
+// }// // Test the functions
+// (async function testFunctions() {
+//     console.log('>>>> Starting fn2');
+//     await fn2();
+//     console.log('>>>> fn2 completed');
+// })();// console.log('>>>> last');
